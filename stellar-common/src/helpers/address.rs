82ecdaa494ef3e15975280d8336ded68b3dcf7ca
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2025-02-13 21:01:55 -0500

    updating foundational module for stellar

--- stellar-common/src/helpers/address.rs
+++ stellar-common/src/helpers/address.rs
@@ -1,4 +1,4 @@
-use stellar_xdr::curr::AccountId;
+use stellar_xdr::curr::{AccountId, PoolId};
 use substreams::Hex;
 
 use crate::pb::sf::substreams::stellar::r#type::v1::{
@@ -8,5 +8,11 @@ use crate::pb::sf::substreams::stellar::r#type::v1::{
 pub fn create_account_address(addr: &AccountId) -> Address {
     Address {
         address_type: Some(address::AddressType::AccountAddress(addr.to_string())),
+    }
+}
+
+pub fn create_account_address_from_pool_id(pool_id: &PoolId) -> Address {
+    Address {
+        address_type: Some(address::AddressType::AccountAddress(pool_id.0.to_string())),
     }
 }
--- stellar-common/src/token_transfer.rs
+++ stellar-common/src/token_transfer.rs
@@ -4,7 +4,7 @@ use crate::{
     pb::sf::{
         stellar::r#type::v1::Block,
         substreams::stellar::r#type::v1::{
-            address, token_transfer::Event, Address, EventMeta, TokenTransfer, TokenTransfers, Transfer,
+            address, token_transfer::Event, Address, Clawback, EventMeta, TokenTransfer, TokenTransfers, Transfer,
         },
     },
     utils,
@@ -96,6 +96,223 @@ fn map_token_transfers(block: Block) -> Result<TokenTransfers, substreams::error
                         amount: BigDecimal::from(payment.amount).into(),
                     })),
                 });
+            }
+            stellar_xdr::curr::OperationBody::CreateClaimableBalance(create_claim_balance) => {
+                let asset = utils::match_asset_code(&create_claim_balance.asset);
+                // let to = create_claim_balance.claimants.first().unwrap();
+                token_transfers.token_transfer_events.push(TokenTransfer {
+                    meta: Some(helpers::event_meta::create_event_meta(
+                        block.number,
+                        block.created_at,
+                        &hash,
+                    )),
+                    asset: Some(asset),
+                    event: Some(Event::Transfer(Transfer {
+                        from: Some(helpers::address::create_account_address(
+                            &trx.source_account.clone().account_id(),
+                        )),
+                        to: None, // TODO: what to do here?
+                        amount: BigDecimal::from(create_claim_balance.amount).into(),
+                    })),
+                });
+            }
+            stellar_xdr::curr::OperationBody::ClaimClaimableBalance(claim_claim_balance) => {
+                // TODO: does the transfer only occur when the claimable balance is claimed?
+            }
+            stellar_xdr::curr::OperationBody::Clawback(clawback) => {
+                let asset = utils::match_asset_code(&clawback.asset);
+                // let to = create_claim_balance.claimants.first().unwrap();
+                token_transfers.token_transfer_events.push(TokenTransfer {
+                    meta: Some(helpers::event_meta::create_event_meta(
+                        block.number,
+                        block.created_at,
+                        &hash,
+                    )),
+                    asset: Some(asset),
+                    event: Some(Event::Clawback(Clawback {
+                        from: Some(helpers::address::create_account_address(
+                            &trx.source_account.clone().account_id(),
+                        )),
+                        amount: BigDecimal::from(clawback.amount).into(),
+                    })),
+                });
+            }
+            stellar_xdr::curr::OperationBody::ClawbackClaimableBalance(clawback_claimable_balance) => {
+                // TODO: same logic here, does the clawback only occur when the claimable balance is claimed?
+            }
+            stellar_xdr::curr::OperationBody::AllowTrust(allow_trust) => {
+                // TODO: how can I fetch the amount here?
+            }
+            stellar_xdr::curr::OperationBody::SetTrustLineFlags(set_trust_line_flags) => {
+                // TODO: how can I fetch the amount here?
+            }
+            stellar_xdr::curr::OperationBody::LiquidityPoolDeposit(liquidity_pool_deposit) => {
+                // TODO: here to create the 2 transfers, I need to know the assets that are present in the pool?
+                //  How can I fetch the asset of the pool at runtime?
+                // TODO: if the from account happens to be the issuer of one of the assets being
+                //  moved in the LP, then a Mint event will be emitted instead of a Transfer
+
+                token_transfers.token_transfer_events.push(TokenTransfer {
+                    meta: Some(helpers::event_meta::create_event_meta(
+                        block.number,
+                        block.created_at,
+                        &hash,
+                    )),
+                    asset: None, // TODO: need to fetch the asset from the pool
+                    event: Some(Event::Transfer(Transfer {
+                        from: Some(helpers::address::create_account_address(
+                            &trx.source_account.clone().account_id(),
+                        )),
+                        to: Some(helpers::address::create_account_address_from_pool_id(
+                            &liquidity_pool_deposit.liquidity_pool_id,
+                        )),
+                        amount: BigDecimal::from(liquidity_pool_deposit.max_amount_a).into(), // TODO: also what value should I use here? The min or the max?
+                    })),
+                });
+                token_transfers.token_transfer_events.push(TokenTransfer {
+                    meta: Some(helpers::event_meta::create_event_meta(
+                        block.number,
+                        block.created_at,
+                        &hash,
+                    )),
+                    asset: None, // TODO: need to fetch the asset from the pool
+                    event: Some(Event::Transfer(Transfer {
+                        from: Some(helpers::address::create_account_address(
+                            &trx.source_account.clone().account_id(),
+                        )),
+                        to: Some(helpers::address::create_account_address_from_pool_id(
+                            &liquidity_pool_deposit.liquidity_pool_id,
+                        )),
+                        amount: BigDecimal::from(liquidity_pool_deposit.max_amount_b).into(), // TODO: also what value should I use here? The min or the max?
+                    })),
+                });
+            }
+            stellar_xdr::curr::OperationBody::LiquidityPoolWithdraw(liquidity_pool_withdraw) => {
+                // TODO: here to create the 2 transfers, I need to know the assets that are present in the pool?
+                //  How can I fetch the asset of the pool at runtime?
+                // TODO: If the to account happens to be the issuer of one of the assets being
+                //  moved in the LP, then a Burn event will be emitted instead of a Transfer
+
+                token_transfers.token_transfer_events.push(TokenTransfer {
+                    meta: Some(helpers::event_meta::create_event_meta(
+                        block.number,
+                        block.created_at,
+                        &hash,
+                    )),
+                    asset: None, // TODO: need to fetch the asset from the pool
+                    event: Some(Event::Transfer(Transfer {
+                        from: Some(helpers::address::create_account_address(
+                            &trx.source_account.clone().account_id(),
+                        )),
+                        to: Some(helpers::address::create_account_address_from_pool_id(
+                            &liquidity_pool_withdraw.liquidity_pool_id,
+                        )),
+                        amount: BigDecimal::from(liquidity_pool_withdraw.min_amount_a).into(),
+                    })),
+                });
+                token_transfers.token_transfer_events.push(TokenTransfer {
+                    meta: Some(helpers::event_meta::create_event_meta(
+                        block.number,
+                        block.created_at,
+                        &hash,
+                    )),
+                    asset: None, // TODO: need to fetch the asset from the pool
+                    event: Some(Event::Transfer(Transfer {
+                        from: Some(helpers::address::create_account_address(
+                            &trx.source_account.clone().account_id(),
+                        )),
+                        to: Some(helpers::address::create_account_address_from_pool_id(
+                            &liquidity_pool_withdraw.liquidity_pool_id,
+                        )),
+                        amount: BigDecimal::from(liquidity_pool_withdraw.min_amount_b).into(),
+                    })),
+                });
+            }
+            stellar_xdr::curr::OperationBody::ManageBuyOffer(manage_buy_offer) => {
+                // TODO: If the from account happens to be the issuer of one of the assets being
+                //  traded, then a Mint event will be emitted instead of a Transfer
+                // TODO: If the to account happens to be the issuer of one of the assets being
+                //  traded, then a Burn event will be emitted instead of a Transfer
+
+                // Also how we handle the from and to accounts?
+                // How do we handle the price?
+            }
+            stellar_xdr::curr::OperationBody::ManageSellOffer(manage_sell_offer) => {
+                // TODO: If the from account happens to be the issuer of one of the assets being
+                //  traded, then a Mint event will be emitted instead of a Transfer
+                // TODO: If the to account happens to be the issuer of one of the assets being
+                //  traded, then a Burn event will be emitted instead of a Transfer
+
+                // Also how we handle the from and to accounts?
+                // How do we handle the price?
+            }
+            stellar_xdr::curr::OperationBody::CreatePassiveSellOffer(create_passive_sell) => {
+                // TODO: If the from account happens to be the issuer of one of the assets being
+                //  traded, then a Mint event will be emitted instead of a Transfer
+                // TODO: If the to account happens to be the issuer of one of the assets being
+                //  traded, then a Burn event will be emitted instead of a Transfer
+
+                // Also how we handle the from and to accounts?
+            }
+            stellar_xdr::curr::OperationBody::PathPaymentStrictSend(path_payment_strict_send) => {
+                let send_asset = utils::match_asset_code(&path_payment_strict_send.send_asset);
+                let dest_asset = utils::match_asset_code(&path_payment_strict_send.dest_asset);
+                if send_asset == dest_asset {
+                    // This is considered as a normal transfer
+                    token_transfers.token_transfer_events.push(TokenTransfer {
+                        meta: Some(helpers::event_meta::create_event_meta(
+                            block.number,
+                            block.created_at,
+                            &hash,
+                        )),
+                        asset: Some(send_asset),
+                        event: Some(Event::Transfer(Transfer {
+                            from: Some(helpers::address::create_account_address(
+                                &trx.source_account.clone().account_id(),
+                            )),
+                            to: Some(helpers::address::create_account_address(
+                                &path_payment_strict_send.clone().destination.account_id(),
+                            )),
+                            amount: BigDecimal::from(path_payment_strict_send.send_amount).into(),
+                        })),
+                    });
+                }
+                // TODO: If the from account happens to be the issuer of one of the assets being
+                //  traded, then a Mint event will be emitted instead of a Transfer
+                // TODO: If the to account happens to be the issuer of one of the assets being
+                //  traded, then a Burn event will be emitted instead of a Transfer
+
+                // Also how we handle the from and to accounts?
+            }
+            stellar_xdr::curr::OperationBody::PathPaymentStrictReceive(path_payment_strict_receive) => {
+                let send_asset = utils::match_asset_code(&path_payment_strict_receive.send_asset);
+                let dest_asset = utils::match_asset_code(&path_payment_strict_receive.dest_asset);
+                if send_asset == dest_asset {
+                    // This is considered as a normal transfer
+                    token_transfers.token_transfer_events.push(TokenTransfer {
+                        meta: Some(helpers::event_meta::create_event_meta(
+                            block.number,
+                            block.created_at,
+                            &hash,
+                        )),
+                        asset: Some(send_asset),
+                        event: Some(Event::Transfer(Transfer {
+                            from: Some(helpers::address::create_account_address(
+                                &trx.source_account.clone().account_id(),
+                            )),
+                            to: Some(helpers::address::create_account_address(
+                                &path_payment_strict_receive.clone().destination.account_id(),
+                            )),
+                            amount: BigDecimal::from(path_payment_strict_receive.dest_amount).into(),
+                        })),
+                    });
+                }
+                // TODO: If the from account happens to be the issuer of one of the assets being
+                //  traded, then a Mint event will be emitted instead of a Transfer
+                // TODO: If the to account happens to be the issuer of one of the assets being
+                //  traded, then a Burn event will be emitted instead of a Transfer
+
+                // Also how we handle the from and to accounts?
             }
             _ => {}
         });
